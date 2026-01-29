8b6bc1cfad847957274215efa8e28f0d9972f92a
Maruthi22k <maruthi22k@gmail.com>
2026-01-29 23:08:02 +0530

    new changes

---
+++ shellscript/03-shellscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+age=25
+color="white"
+echo "please pass input ="
+read USER_NAME
+
+echo "username is ${USER_NAME}"
+read -p "Enter your age: $age"
+
+echo " please give tell me your faverate color: $color"
