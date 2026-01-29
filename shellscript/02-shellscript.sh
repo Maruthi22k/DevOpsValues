702467d191045c3adb1d1b2c4e069dd90b814a82
Maruthi22k <maruthi22k@gmail.com>
2026-01-29 22:54:41 +0530

    new changes

--- shellscript/02-shellscript.sh
+++ shellscript/02-shellscript.sh
@@ -9,3 +9,10 @@ echo "$Friend:: welcome to shell scripts and also to DevOPs"
 echo "$PERSION:: you are my friend $Friend to shell scripts"
 echo "$Friend:: welcome to shell scripts"
 
+PERSON1=$1 # no space between value and =
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
+echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
+echo "$PERSON2:: I am learning Shell. What about you?"
