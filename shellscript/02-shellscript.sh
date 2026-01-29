c3ed368edbd67e2f56f01f35a4f866fcaa883561
Maruthi22k <maruthi22k@gmail.com>
2026-01-29 22:50:35 +0530

    new changes

---
+++ shellscript/02-shellscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSION=$1
+DevOPsPPP=$2
+Friend=$3
+
+echo "$DevOPsPPP:: welcome to shell $PERSION scripts"
+echo "$Friend:: welcome to shell scripts and also to DevOPs"
+echo "$PERSION:: you are my friend $Friend to shell scripts"
+echo "$Friend:: welcome to shell scripts"
