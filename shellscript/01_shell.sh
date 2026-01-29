6e1a603445119b7fa3d08d9e33289d2574f6a3b7
Maruthi22k <maruthi22k@gmail.com>
2026-01-29 22:37:43 +0530

    new changes

--- shellscript/01_shell.sh
+++ shellscript/01_shell.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "welcpome to shell scripts"
+
+
+echo "Maruthi:: welcome to shell scripts"
+echo "Anusha:: welcome to shell scripts and also to DevOPs"
+echo "Sridhatri:: welcome to shell scripts"
+echo "SaiSidvik:: welcome to shell scripts"
