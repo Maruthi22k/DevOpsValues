fa83f514499d46f87cff51c70da4d53156059b20
Maruthi22k <maruthi22k@gmail.com>
2026-01-31 21:53:30 +0530

    new changes

--- shellscript/02-shellscript.sh
+++ shellscript/02-shellscript.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-PERSION=$1
-DevOPsPPP=$2
-Friend=$3
-
-echo "$DevOPsPPP:: welcome to shell $PERSION scripts"
-echo "$Friend:: welcome to shell scripts and also to DevOPs"
-echo "$PERSION:: you are my friend $Friend to shell scripts"
-echo "$Friend:: welcome to shell scripts"
+VAR=$(date)
 
 PERSON1=$1 # no space between value and =
 PERSON2=$2
@@ -15,4 +8,4 @@ PERSON2=$2
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
 echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
-echo "$PERSON2:: I am learning Shell. What about you?"
+echo "$PERSON2:: I am learning Shell. What is today date=?$VAR"
