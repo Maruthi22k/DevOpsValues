40ce5e96038e5d16c94e21ecdb2e7bc77486f36d
Maruthi22k <maruthi22k@gmail.com>
2026-02-01 22:18:39 +0530

    new changes

--- shellscript/dataTypes01.sh
+++ shellscript/dataTypes01.sh
@@ -12,6 +12,7 @@ DATATYPE=(Maruthi,Anusha,SaiSidvik)
 echo "Names are : $DATATYPE"
 
 echo "all names are: ${DATATYPE[@]}"
+echo "all names are: ${DATATYPE[*]}"
 #echo "First Fruit is: ${FRUITS[0]}"
 #echo "Second Fruit is: ${FRUITS[1]}"
 #echo "Third Fruit is: ${FRUITS[2]}"
