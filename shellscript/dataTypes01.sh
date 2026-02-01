e3c060639a6ce0fae22a8f97a7847c879f4fe93c
Maruthi22k <maruthi22k@gmail.com>
2026-02-01 22:23:52 +0530

    new changes

--- shellscript/dataTypes01.sh
+++ shellscript/dataTypes01.sh
@@ -8,12 +8,12 @@ num2=maruthi
 sum=$((num1+num2))
 echo "sum is : $sum"
 
-DATATYPE=(Maruthi,Anusha,SaiSidvik)
+DATATYPE=(Maruthi, Anusha, SaiSidvik)
 echo "Names are : $DATATYPE"
 
 echo "all names are: ${DATATYPE[@]}"
 echo "all names using * : ${DATATYPE[*]}"
 echo "all names are: ${DATATYPE[#]}"
-echo "First Fruit is: ${DATATYPE[0]}"
-echo "Second Fruit is: ${DATATYPE[1]}"
-echo "Third Fruit is: ${DATATYPE[2]}"
+echo "First Name is: ${DATATYPE[0]}"
+echo "Second Name is: ${DATATYPE[1]}"
+echo "Third Name is: ${DATATYPE[2]}"
