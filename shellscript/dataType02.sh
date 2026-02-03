2e8d49b39b73eb9abccffa3b95f28ead610f1cea
Maruthi22k <maruthi22k@gmail.com>
2026-02-03 15:17:19 +0530

    new changes

--- shellscript/dataType02.sh
+++ shellscript/dataType02.sh
@@ -2,10 +2,18 @@
 
 Number=$10
 
-if [ $Number -eq 50 ]
+if [ $Number -lt 10 ]
 then
 echo "Number is greater than 50"
-else 
-echo "Number is less thgan 30"
+
+else if [ $Number -gt 40] 
+echo "Number is less than 40"
+
+else if [ $Number -eq 16] 
+echo "Number is equal to 16"
+
+else
+echo "other position values"
+
 fi
  
