608c6091bdefa5919ad4dfef9d00fc2557a5bc7a
Maruthi22k <maruthi22k@gmail.com>
2026-02-03 13:49:10 +0530

    new changes

---
+++ shellscript/timeCheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+
+echo "Start time is = $START_TIME"
+
+END_TIME=$(date +%s)
+
+echo "End time is = $END_TIME"
+
+Total_time=$((END_TIME-START_TIME))
+
+echo "Total Time taken to Execute = $Total_time"
