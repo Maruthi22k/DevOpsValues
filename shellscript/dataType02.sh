b999299aece8f8df7596c06c828aa9f60153ca87
Maruthi22k <maruthi22k@gmail.com>
2026-02-03 14:16:55 +0530

    new changes

---
+++ shellscript/dataType02.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Number=$10
+
+if [ $Number -gt 50 ]
+then
+echo "Number is greater than 50"
+else 
+echo "Number is less thgan 30"
+fi
+ 
--- shellscript/datatypes.sh
+++ shellscript/datatypes.sh
@@ -3,13 +3,15 @@
 
 age=30
 age1=38
-sumage=$((age1+age))
+sumage=$(($age1+$age))
 
 echo "age details: $sumage"
+sleep 5
+
 
 age=30
 name=maruthi
-agedetails=$((age1)+$(name))
+agedetails=$(($age1+$age))
 
 echo "age details: $agedetails"
 
