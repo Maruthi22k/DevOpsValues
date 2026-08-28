120918d466b755110b25965dc5e22e24319968cd
JCSDev2025 <Anusha@JCSGlobal.in>
2026-08-28 22:25:34 +0530

    all date

--- shellprogram/TestingShell/01-cpu.sh
+++ shellprogram/TestingShell/01-cpu.sh
@@ -7,4 +7,9 @@ if [[ $# -ne 2 ]];then
 
     echo "Usage: validate.sh [username] [password]"
     exit 1
+elif [[ $1 == "admin" && $2 == "password" ]]; then
+    echo "Login successful!"
+else
+    echo "Invalid username or password."
+    exit 1
 fi
