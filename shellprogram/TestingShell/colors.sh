98907d6361a94ca573585006117854cf53947996
JCSDev2025 <Anusha@JCSGlobal.in>
2026-08-29 15:50:11 +0530

    all date

---
+++ shellprogram/TestingShell/colors.sh
@@ -0,0 +1,12 @@
+# !#/bin/bash
+
+R=\e[31m   ## Red color code
+G=\e[32m  ## Green color code
+Y=\e[33m  ## Yellow color code
+B=\e[34m  ## Blue color code
+M=\e[35m  ## Magenta color code
+C=\e[36m  ## Cyan color code
+W=\e[37m  ## White color code
+
+USERID=$(id -u)
+echo -e "${C}print the current user id: $USERID${W}"
