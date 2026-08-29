f905cd84a84b74ebb440134597abdf0a5509e5a2
JCSDev2025 <Anusha@JCSGlobal.in>
2026-08-29 16:11:31 +0530

    all date

--- shellprogram/TestingShell/colors.sh
+++ shellprogram/TestingShell/colors.sh
@@ -9,4 +9,28 @@ C=\e[36m  ## Cyan color code
 W=\e[37m  ## White color code
 
 USERID=$(id -u)
-echo -e "$R print the current user id: $USERID $N"
+
+if [$USERID -eq 0];then
+    echo -e "$G This is the root user $N"
+    exit 1
+fi
+
+VALIDATE (){
+    if [$1 -eq 0];then
+        echo -e "$G $2 ... SUCCESS $N"
+    else
+        echo -e "$R $3 ... FAILURE $N"
+    fi
+}
+
+
+dnf list installed | grep nginx  ## meaning of this command is to check whether nginx is installed or not. If it is installed then it will return the package name and version otherwise it will return nothing.
+dnf install nginx -y
+if [$? -ne 0];then
+    echo -e "$R Installing Nginx ... FAILURE $N"
+    VALIDATE $? "Installing Nginx ... FAILURE"
+    exit 1
+else
+    echo -e "$G Installing already installed Nginx ...  $N"
+    VALIDATE 0 "Installing Nginx ... SUCCESS"
+fi
